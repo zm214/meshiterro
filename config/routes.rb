a0428e008ab1cdff37afb9e86ca4ae424fbfbfa2
Ubuntu <ubuntu@ip-172-31-20-236.ap-northeast-1.compute.internal>
2026-09-16 12:23:17 +0000

    [Finish]2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :index, :show]
+  
   resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
   resource :session
   resources :passwords, param: :token
