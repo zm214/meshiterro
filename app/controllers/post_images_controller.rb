f00788dd211157f08c2e8291412d411099f15fc4
Ubuntu <ubuntu@ip-172-31-20-236.ap-northeast-1.compute.internal>
2026-09-20 14:38:10 +0000

    [Finish]投稿機能5-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -15,7 +15,14 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
+
+   def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
+   end
 
    private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
   resource :session
   resources :passwords, param: :token
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
